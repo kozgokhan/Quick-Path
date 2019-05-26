2d79fce4ff44acded622b703c093c53f8eca580a
Kaan Ozgokhan <kozgokhan@yahoo.com>
2019-05-27 00:15:57 +0200

    update

--- qpremove.m
+++ qpremove.m
@@ -1,1 +1,22 @@
 % https://www.mathworks.com/matlabcentral/answers/383592-how-to-remove-some-lines-of-a-file
+
+fid=fopen('qpdatabase.pdb');
+fff = textscan(fid,'%s','Delimiter','\n');
+fclose(fid);
+
+name = 'testilop';
+
+qpdb = qpf_dbread();
+[isFound, rowNum, ~] = qpf_searchName(qpdb,name);
+
+if isFound
+   fff{1,1}(rowNum) = [];
+end
+
+fff{1,1}
+
+fid = fopen('qpdatabase.pdb');
+fprintf(fid, '%s\n', fff{:});
+fclose(fid);
+
+qplist
